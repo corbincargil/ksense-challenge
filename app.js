3092e3d1de8f73c0e04e50d268f6a7e34b198e19
corbincargil <corbin.cargil@gmail.com>
2022-10-26 15:56:33 -0500

    added users to table

--- app.js
+++ app.js
@@ -10,34 +10,6 @@ const postContainer = document.createElement("div");
 root.appendChild(tableContainer);
 // console.log(root);
 
-//getUsers function
-async function getUsers() {
-  try {
-    const results = await fetch(
-      "https://jsonplaceholder.typicode.com/users/"
-    ).then((response) => response.json());
-    console.log(results);
-    return results;
-  } catch (error) {
-    console.log(error);
-  }
-}
-//const users = getUsers();
-
-//getPosts function
-async function getPosts() {
-  try {
-    const results = await fetch(
-      "https://jsonplaceholder.typicode.com/posts/"
-    ).then((response) => response.json());
-    console.log(results);
-    return results;
-  } catch (error) {
-    console.log(error);
-  }
-}
-const posts = getPosts();
-
 //Create table
 const createTable = (container) => {
   let table = document.createElement("table");
@@ -54,3 +26,53 @@ const createTable = (container) => {
 };
 
 createTable(tableContainer);
+
+//getUsers function
+async function getUsers() {
+  try {
+    const results = await fetch(
+      "https://jsonplaceholder.typicode.com/users/"
+    ).then((response) => response.json());
+    return results;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//render users in table
+const renderTableData = async () => {
+  const table = document.getElementById("user-table");
+  const users = await getUsers();
+  users.forEach((user) => {
+    let tr = document.createElement("tr");
+    let id = document.createElement("td");
+    let username = document.createElement("td");
+    let company = document.createElement("td");
+    id.textContent = `${user.id}`;
+    username.textContent = `${user.username}`;
+    company.textContent = `${user.company.name}`;
+    tr.appendChild(id);
+    tr.appendChild(username);
+    tr.appendChild(company);
+    tr.addEventListener("click", () => {
+      console.log(`You clicked ${user.username}`);
+    });
+    tr.classList.add("list-item");
+    table.appendChild(tr);
+  });
+};
+renderTableData();
+
+//getPosts function
+async function getPosts() {
+  try {
+    const results = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/"
+    ).then((response) => response.json());
+    console.log(results);
+    return results;
+  } catch (error) {
+    console.log(error);
+  }
+}
+// const posts = getPosts();
