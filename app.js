f60d1b40e12f245ac4146c8f49ea14a9aa96de16
corbincargil <corbin.cargil@gmail.com>
2022-10-26 17:03:30 -0500

    clicking on a post title now displays the content in the postPreview container. Styling updates. Challenge completed.

--- app.js
+++ app.js
@@ -9,18 +9,24 @@ import getPosts from "./getPosts.js";
 const root = document.getElementById("root");
 const tableContainer = document.createElement("div");
 const postContainer = document.createElement("div");
+const postHeadingContainer = document.createElement("div");
 const postHeading = document.createElement("h2");
+const postSubHeading = document.createElement("p");
 const postList = document.createElement("ol");
 const postPreview = document.createElement("div");
 tableContainer.setAttribute("id", "table-container");
 postContainer.setAttribute("id", "post-container");
+postHeadingContainer.setAttribute("id", "post-heading-container");
 postHeading.setAttribute("id", "post-heading");
+postSubHeading.setAttribute("id", "post-subheading");
 postList.setAttribute("id", "post-list");
 postPreview.setAttribute("id", "post-preview");
 postHeading.textContent = "Click a user in the table";
+postSubHeading.textContent = "Click a title to preview the post";
 postList.textContent = "Post titles here...";
 postPreview.textContent = "Post content here...";
-postContainer.appendChild(postHeading);
+postHeadingContainer.appendChild(postHeading);
+postContainer.appendChild(postHeadingContainer);
 postContainer.appendChild(postList);
 postContainer.appendChild(postPreview);
 root.appendChild(tableContainer);
@@ -58,8 +64,10 @@ const renderTableData = async () => {
     tr.appendChild(company);
     tr.addEventListener("click", () => {
       clearPostList();
+      postPreview.textContent = "Post content here...";
       renderPosts(user.id);
       postHeading.textContent = `${user.username}'s Posts:`;
+      postHeadingContainer.appendChild(postSubHeading);
     });
     tr.classList.add("list-item");
     table.appendChild(tr);
@@ -74,7 +82,10 @@ async function renderPosts(id) {
     let li = document.createElement("li");
     li.textContent = `${post.title}`;
     li.addEventListener("click", () => {
-      console.log(`you clicked the post`);
+      clearSelectedPost();
+      li.classList.remove("unselected");
+      li.classList.add("selected");
+      postPreview.textContent = `"${post.body}"`;
     });
     list.appendChild(li);
   });
@@ -83,6 +94,15 @@ async function renderPosts(id) {
 //Clean list of posts
 const clearPostList = () => {
   postList.innerHTML = "";
+};
+
+//Clear selected post
+const clearSelectedPost = () => {
+  let array = [...postList.children];
+  array.forEach((post) => {
+    post.classList.remove("selected");
+    post.classList.add("unselected");
+  });
 };
 
 renderTableData();
